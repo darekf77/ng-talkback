fcb256cafcb3c8d7c18ba60035bbcabe4c7bf34e
Dariusz Filipiak <darekf77@gmail.com>
2023-06-27 06:17:17 +0200

    new version 16.1.3

--- mocha-setup.js
+++
@@ -1,7 +0,0 @@
-require("ts-node/register")
-require("source-map-support/register")
-const chai = require("chai");
-const td = require("testdouble");
-
-global.expect = chai.expect;
-global.td = td;
