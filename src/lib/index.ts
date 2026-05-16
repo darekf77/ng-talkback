8bda1e71f66802552b2e3d3e397d95dd06747941
Dariusz Filipiak <darekf77@gmail.com>
2026-05-16 21:28:26 +0200

    $$$ update $$$

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,27 +1,36 @@
 export type DummyASDASDASd = 'a';
-let a = 23;
+let a = 234343;
 a++;
+
+//#region @browser
+console.log('sdasdasd');
+//#endregion
 
 //#region @backend
 export * from './options.backend';
 export * from './tape.backend';
 
+a++;
 
-import TalkbackFactory from './talkback-factory.backend'
-import Options, { DefaultOptions, FallbackMode, RecordMode } from './options.backend'
-
+import TalkbackFactory from './talkback-factory.backend';
+import Options, {
+  DefaultOptions,
+  FallbackMode,
+  RecordMode,
+} from './options.backend';
 
 const talkbackFn = (options: Partial<Options>) => {
-  return TalkbackFactory.server(options)
-}
+  return TalkbackFactory.server(options);
+};
 
 talkbackFn.Options = {
   Default: DefaultOptions,
   FallbackMode,
-  RecordMode
-}
+  RecordMode,
+};
 
-talkbackFn.requestHandler = (options: Partial<Options>) => TalkbackFactory.requestHandler(options)
+talkbackFn.requestHandler = (options: Partial<Options>) =>
+  TalkbackFactory.requestHandler(options);
 
 export const talkback = talkbackFn;
 export default talkbackFn;
